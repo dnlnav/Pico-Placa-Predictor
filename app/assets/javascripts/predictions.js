cafeba49b68c422634e22fd57041a1c81df17eca
Daniel Navarrete <cdng9202@gmail.com>
2018-06-11 01:55:16 -0500

    Finish layout

--- app/assets/javascripts/predictions.js
+++ app/assets/javascripts/predictions.js
@@ -1,7 +1,21 @@
 $(function(){
-    $(".prediction-form").on("ajax:success", function (data, status, xhr) {
+    disableButton();
+    $(".predictor__form").on("ajax:success", function (data, status, xhr) {
         $('#result').text(status.message);
     }).on("ajax:error", function (ajaxrequest, ajaxOptions, thrownError) {
         console.log(thrownError);
     });
+
+    $('#license_plate, #date, #time').on('change', disableButton);
+
+    function disableButton(){
+        let emptyPlate = $('#license_plate').val() == '';
+        let emptyDate = $('#date').val() == '';
+        let emptyTime = $('#time').val() == '';
+        if (emptyPlate || emptyDate || emptyTime){
+            $('.predictor__btn').attr('disabled', 'disabled');
+        } else {
+            $('.predictor__btn').removeAttr('disabled');
+        }
+    }
 });
